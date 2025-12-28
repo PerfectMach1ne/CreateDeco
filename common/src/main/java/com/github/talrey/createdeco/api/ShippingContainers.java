c7e2c21f0a93308185ff6f3c96b20a4e564339aa
starr4ever@TransEdenV4 <starflowerr777@gmail.com>
2025-12-28 20:09:02 +0100

    prep
    Signed-off-by: starr4ever@TransEdenV4 <starflowerr777@gmail.com>
    fix: ItemLike cast crash @ Window
    experience: hatred for VS Code and its shitty little annoying plugins and linters so great I might switch to coding on vi tomorrow.
    note: runDatagen is still broken, likely Registrate's fault

--- common/src/main/java/com/github/talrey/createdeco/api/ShippingContainers.java
+++ common/src/main/java/com/github/talrey/createdeco/api/ShippingContainers.java
@@ -14,8 +14,11 @@ import com.simibubi.create.foundation.data.SharedProperties;
 import com.tterrag.registrate.builders.BlockBuilder;
 import com.tterrag.registrate.providers.DataGenContext;
 import com.tterrag.registrate.providers.RegistrateRecipeProvider;
+import com.tterrag.registrate.util.entry.RegistryEntry;
+
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.core.Registry;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
@@ -26,59 +29,51 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import org.apache.commons.lang3.text.WordUtils;
-
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.stream.Collectors;
 
 import static com.simibubi.create.foundation.data.CreateRegistrate.connectedTextures;
 import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
 
 public class ShippingContainers {
-  public static BlockBuilder<ShippingContainerBlock,?> build (
-    CreateRegistrate reg, DyeColor color
-  ) {
-    String regName = color.getName() + "_shipping_container";
-    String visName = WordUtils.capitalizeFully(color.getName().replaceAll("_", " "));
-
-    return reg.block(regName, p -> new ShippingContainerBlock(p, color))
-      .initialProperties(SharedProperties::softMetal)
-      .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
-//      .properties(p -> p.mapColor(DyeColor.valueOf(color)))
-      .properties(p -> p.sound(SoundType.NETHERITE_BLOCK).explosionResistance(1200))
-      .transform(pickaxeOnly())
-      .item(ShippingContainerBlockItem::new)
-        .build()
-      .tag(BlockTags.MINEABLE_WITH_PICKAXE)
-      //String visName =  color.name().charAt(0) + color.name().substring(1).toLowerCase().replaceAll("_", " ")
-      .lang(visName + " Shipping Container")
-
-      .blockstate((ctx, prov) -> BlockStateGenerator.shippingContainer(CreateDecoMod.REGISTRATE, color, ctx, prov))
-      .transform(MountedItemStorageType.mountedItemStorage(AllMountedStorageTypes.VAULT))
-      .onRegister(connectedTextures(ShippingContainerCTBehavior::new));
-  }
-
-  public static <T extends Block> void recipeCrafting (DyeColor color, DataGenContext<Block, T> ctx, RegistrateRecipeProvider prov) {
-    ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, ctx.get())
-      .pattern("CS")
-      .pattern("SB")
-      .define('S', AllItems.IRON_SHEET)
-      .define('C', DyeItem.byColor(color))
-      .define('B', Items.BARREL)
-      .unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(
-        ItemPredicate.Builder.item().of(AllItems.IRON_SHEET).build()
-      ))
-      .save(prov, color.getName() + "_shipping_container");
-  }
-
-  public static <T extends Block> void recipeDyeing (DyeColor color, DataGenContext<Block, T> ctx, RegistrateRecipeProvider prov) {
-    ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, ctx.get())
-      .requires(DyeItem.byColor(color))
-      .requires(AllBlocks.ITEM_VAULT.asItem())
-      .unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(
-        ItemPredicate.Builder.item().of(AllBlocks.ITEM_VAULT.asItem()).build()
-      ))
-      .save(prov, color.getName() + "_shipping_container_from_dyeing_vaults");
-  }
+	public static BlockBuilder<ShippingContainerBlock,?> build (
+		CreateRegistrate reg, DyeColor color
+	) {
+		String regName = color.getName() + "_shipping_container";
+		
+		return reg.block(regName, p -> new ShippingContainerBlock(p, color))
+		.initialProperties(SharedProperties::softMetal)
+		.properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
+		//      .properties(p -> p.mapColor(DyeColor.valueOf(color)))
+		.properties(p -> p.sound(SoundType.NETHERITE_BLOCK).explosionResistance(1200))
+		.transform(pickaxeOnly())
+		.item(ShippingContainerBlockItem::new)
+		.build()
+		.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+		.lang(color.getName().charAt(0) + color.name().substring(1).toLowerCase().replaceAll("_", " ") + " Shipping Container")
+		.blockstate((ctx, prov) -> BlockStateGenerator.shippingContainer(CreateDecoMod.REGISTRATE, color, ctx, prov))
+		.transform(MountedItemStorageType.mountedItemStorage(AllMountedStorageTypes.VAULT))
+		.onRegister(connectedTextures(ShippingContainerCTBehavior::new));
+	}
+	
+	public static <T extends Block> void recipeCrafting (DyeColor color, DataGenContext<Block, T> ctx, RegistrateRecipeProvider prov) {
+		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, ctx.get())
+		.pattern("CS")
+		.pattern("SB")
+		.define('S', AllItems.IRON_SHEET)
+		.define('C', DyeItem.byColor(color))
+		.define('B', Items.BARREL)
+		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(
+			ItemPredicate.Builder.item().of(AllItems.IRON_SHEET).build()
+		))
+		.save(prov, color.getName() + "_shipping_container");
+	}
+	
+	public static <T extends Block> void recipeDyeing (DyeColor color, DataGenContext<Block, T> ctx, RegistrateRecipeProvider prov) {
+		ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, ctx.get())
+		.requires(DyeItem.byColor(color))
+		.requires(AllBlocks.ITEM_VAULT.asItem())
+		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(
+			ItemPredicate.Builder.item().of(AllBlocks.ITEM_VAULT.asItem()).build()
+		))
+		.save(prov, color.getName() + "_shipping_container_from_dyeing_vaults");
+	}
 }
--- common/src/main/java/com/github/talrey/createdeco/api/Windows.java
+++ common/src/main/java/com/github/talrey/createdeco/api/Windows.java
@@ -29,7 +29,6 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -42,119 +41,120 @@ import java.util.function.Supplier;
 import static com.simibubi.create.foundation.data.CreateRegistrate.connectedTextures;
 
 public class Windows {
-  private static BlockBehaviour.Properties glassProperties(BlockBehaviour.Properties p) {
-    return p.isValidSpawn(Windows::never)
-        .isRedstoneConductor(Windows::never)
-        .isSuffocating(Windows::never)
-        .isViewBlocking(Windows::never);
-  }
-
-  private static boolean never(BlockState state, BlockGetter getter, BlockPos pos) {
-    return false;
-  }
-
-  private static Boolean never(BlockState state, BlockGetter getter, BlockPos pos, EntityType<?> type) {
-    return false;
-  }
-
-  public static BlockEntry<WindowBlock> metalWindowBlock(String metal) {
-    return metalWindowBlock(metal, () -> RenderType::cutoutMipped, false);
-  }
-
-  public static BlockEntry<WindowBlock> metalWindowBlock(String metal,
-                                                          Supplier<Supplier<RenderType>> renderType, boolean translucent) {
-    String name = metal + "_window";
-    NonNullFunction<String, ResourceLocation> end_texture = $ -> CreateDecoMod.id(palettesDir() + $);
-    NonNullFunction<String, ResourceLocation> side_texture = n -> CreateDecoMod.id(palettesDir() + n);
-    return windowBlock(name, () -> SpriteShifts.METAL_WINDOWS.get(metal), renderType,
-        translucent, end_texture, side_texture, Blocks.GLASS::defaultMapColor);
-  }
-
-
-  public static BlockEntry<WindowBlock> windowBlock(String name,
-                                                    Supplier<CTSpriteShiftEntry> ct, Supplier<Supplier<RenderType>> renderType, boolean translucent,
-                                                    NonNullFunction<String, ResourceLocation> endTexture, NonNullFunction<String, ResourceLocation> sideTexture,
-                                                    Supplier<MapColor> color) {
-    return CreateDecoMod.REGISTRATE.block(name.toLowerCase(Locale.ROOT).replace(" ", "_"), p -> new WindowBlock(p, translucent))
-        //.onRegister(connectedTextures(() -> new HorizontalCTBehaviour(ct.get())))
-        .onRegister(CreateRegistrate.connectedTextures(() ->
-            new HorizontalCTBehaviour(SpriteShifts.METAL_WINDOWS.get(name.replace("_window", ""))
-        )))
-        .addLayer(renderType)
-        .recipe((c, p) -> ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, c.get(), 2)
-            .pattern(" # ")
-            .pattern("#X#")
-            .define('#', Ingredient.of(CreateDecoTags.ingot(name.replace("_window", ""))))
-            .define('X', CreateDecoTags.GLASS)
-            .unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(
-              (ItemLike) ItemPredicate.Builder.item().of(CreateDecoTags.ingot(name.replace("_window", "")))
-            ))
-            .save(p::accept))
-        .initialProperties(() -> Blocks.GLASS)
-        .properties(Windows::glassProperties)
-        .properties(p -> p.mapColor(color.get()))
-        .loot((t, g) -> t.dropWhenSilkTouch(g))
-        .blockstate((ctx,prov)-> BlockStateGenerator.window(ctx, prov, sideTexture, endTexture))
-        .tag(BlockTags.IMPERMEABLE)
-        .simpleItem()
-        .register();
-  }
-
-  public static BlockEntry<ConnectedGlassPaneBlock> metalWindowPane(String metal,
-                                                                     Supplier<? extends Block> parent) {
-    return metalWindowPane(metal, parent, () -> RenderType::cutoutMipped);
-  }
-
-  public static BlockEntry<ConnectedGlassPaneBlock> metalWindowPane(String metal,
-                                                                     Supplier<? extends Block> parent, Supplier<Supplier<RenderType>> renderType) {
-    String name = metal.toLowerCase(Locale.ROOT).replace(" ", "_") + "_window";
-    ResourceLocation topTexture = CreateDecoMod.id(palettesDir() + name + "_end");
-    ResourceLocation sideTexture = CreateDecoMod.id(palettesDir() + name);
-    return connectedGlassPane(name, parent, () -> SpriteShifts.METAL_WINDOWS.get(metal), sideTexture,
-        sideTexture, topTexture, renderType);
-  }
-
-  private static BlockEntry<ConnectedGlassPaneBlock> connectedGlassPane(String name, Supplier<? extends Block> parent,
-                                                                        Supplier<CTSpriteShiftEntry> ctshift, ResourceLocation sideTexture, ResourceLocation itemSideTexture,
-                                                                        ResourceLocation topTexture, Supplier<Supplier<RenderType>> renderType) {
-    NonNullConsumer<? super ConnectedGlassPaneBlock> connectedTextures =
-        connectedTextures(() -> new GlassPaneCTBehaviour(ctshift.get()));
-    String CGPparents = "block/connected_glass_pane/";
-    String prefix = name + "_pane_";
-
-    return glassPane(name, parent, itemSideTexture, topTexture, ConnectedGlassPaneBlock::new, renderType,
-        connectedTextures, BlockStateGenerator.windowPane(CGPparents, prefix, sideTexture, topTexture));
-  }
-
-  private static <G extends GlassPaneBlock> BlockEntry<G> glassPane(String name, Supplier<? extends Block> parent,
-                                                                    ResourceLocation sideTexture, ResourceLocation topTexture, NonNullFunction<BlockBehaviour.Properties, G> factory,
-                                                                    Supplier<Supplier<RenderType>> renderType, NonNullConsumer<? super G> connectedTextures,
-                                                                    NonNullBiConsumer<DataGenContext<Block, G>, RegistrateBlockstateProvider> stateProvider) {
-    name += "_pane";
-
-    return CreateDecoMod.REGISTRATE.block(name, factory)
-        .onRegister(connectedTextures)
-        .addLayer(renderType)
-        .initialProperties(() -> Blocks.GLASS_PANE)
-        .properties(p -> p.mapColor(parent.get()
-            .defaultMapColor()))
-        .blockstate(stateProvider)
-        .recipe((c, p) -> ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, c.get(), 16)
-            .pattern("###")
-            .pattern("###")
-            .define('#', parent.get())
-            .unlockedBy("has_ingredient", RegistrateRecipeProvider.has(parent.get()))
-            .save(p::accept))
-        .loot((t, g) -> t.dropWhenSilkTouch(g))
-        .item()
-        .model((c, p) -> p.withExistingParent(c.getName(), CreateDecoMod.id("item/pane"))
-            .texture("pane", sideTexture)
-            .texture("edge", topTexture))
-        .build()
-        .register();
-  }
-
-  private static String palettesDir() {
-    return "block/palettes/windows/";
-  }
-}
+	private static BlockBehaviour.Properties glassProperties(BlockBehaviour.Properties p) {
+		return p.isValidSpawn(Windows::never)
+		.isRedstoneConductor(Windows::never)
+		.isSuffocating(Windows::never)
+		.isViewBlocking(Windows::never);
+	}
+	
+	private static boolean never(BlockState state, BlockGetter getter, BlockPos pos) {
+		return false;
+	}
+	
+	private static Boolean never(BlockState state, BlockGetter getter, BlockPos pos, EntityType<?> type) {
+		return false;
+	}
+	
+	public static BlockEntry<WindowBlock> metalWindowBlock(String metal) {
+		return metalWindowBlock(metal, () -> RenderType::cutoutMipped, false);
+	}
+	
+	public static BlockEntry<WindowBlock> metalWindowBlock(String metal,
+		Supplier<Supplier<RenderType>> renderType, boolean translucent) {
+			String name = metal + "_window";
+			NonNullFunction<String, ResourceLocation> end_texture = $ -> CreateDecoMod.id(palettesDir() + $);
+			NonNullFunction<String, ResourceLocation> side_texture = n -> CreateDecoMod.id(palettesDir() + n);
+			return windowBlock(name, () -> SpriteShifts.METAL_WINDOWS.get(metal), renderType,
+			translucent, end_texture, side_texture, Blocks.GLASS::defaultMapColor);
+		}
+		
+		
+		public static BlockEntry<WindowBlock> windowBlock(String name,
+			Supplier<CTSpriteShiftEntry> ct, Supplier<Supplier<RenderType>> renderType, boolean translucent,
+			NonNullFunction<String, ResourceLocation> endTexture, NonNullFunction<String, ResourceLocation> sideTexture,
+			Supplier<MapColor> color) {
+				return CreateDecoMod.REGISTRATE.block(name.toLowerCase(Locale.ROOT).replace(" ", "_"), p -> new WindowBlock(p, translucent))
+				//.onRegister(connectedTextures(() -> new HorizontalCTBehaviour(ct.get())))
+				.onRegister(CreateRegistrate.connectedTextures(() ->
+				new HorizontalCTBehaviour(SpriteShifts.METAL_WINDOWS.get(name.replace("_window", ""))
+			)))
+			.addLayer(renderType)
+			.recipe((c, p) -> ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, c.get(), 2)
+			.pattern(" # ")
+			.pattern("#X#")
+			.define('#', Ingredient.of(CreateDecoTags.ingot(name.replace("_window", ""))))
+			.define('X', CreateDecoTags.GLASS)
+			.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(
+				ItemPredicate.Builder.item().of(CreateDecoTags.ingot(name.replace("_window", ""))).build()
+			))
+			.save(p::accept))
+			.initialProperties(() -> Blocks.GLASS)
+			.properties(Windows::glassProperties)
+			.properties(p -> p.mapColor(color.get()))
+			.loot((t, g) -> t.dropWhenSilkTouch(g))
+			.blockstate((ctx,prov)-> BlockStateGenerator.window(ctx, prov, sideTexture, endTexture))
+			.tag(BlockTags.IMPERMEABLE)
+			.simpleItem()
+			.register();
+		}
+		
+		public static BlockEntry<ConnectedGlassPaneBlock> metalWindowPane(String metal,
+			Supplier<? extends Block> parent) {
+				return metalWindowPane(metal, parent, () -> RenderType::cutoutMipped);
+			}
+			
+			public static BlockEntry<ConnectedGlassPaneBlock> metalWindowPane(String metal,
+				Supplier<? extends Block> parent, Supplier<Supplier<RenderType>> renderType) {
+					String name = metal.toLowerCase(Locale.ROOT).replace(" ", "_") + "_window";
+					ResourceLocation topTexture = CreateDecoMod.id(palettesDir() + name + "_end");
+					ResourceLocation sideTexture = CreateDecoMod.id(palettesDir() + name);
+					return connectedGlassPane(name, parent, () -> SpriteShifts.METAL_WINDOWS.get(metal), sideTexture,
+					sideTexture, topTexture, renderType);
+				}
+				
+				private static BlockEntry<ConnectedGlassPaneBlock> connectedGlassPane(String name, Supplier<? extends Block> parent,
+					Supplier<CTSpriteShiftEntry> ctshift, ResourceLocation sideTexture, ResourceLocation itemSideTexture,
+					ResourceLocation topTexture, Supplier<Supplier<RenderType>> renderType) {
+						NonNullConsumer<? super ConnectedGlassPaneBlock> connectedTextures =
+						connectedTextures(() -> new GlassPaneCTBehaviour(ctshift.get()));
+						String CGPparents = "block/connected_glass_pane/";
+						String prefix = name + "_pane_";
+						
+						return glassPane(name, parent, itemSideTexture, topTexture, ConnectedGlassPaneBlock::new, renderType,
+							connectedTextures, BlockStateGenerator.windowPane(CGPparents, prefix, sideTexture, topTexture));
+						}
+						
+						private static <G extends GlassPaneBlock> BlockEntry<G> glassPane(String name, Supplier<? extends Block> parent,
+							ResourceLocation sideTexture, ResourceLocation topTexture, NonNullFunction<BlockBehaviour.Properties, G> factory,
+							Supplier<Supplier<RenderType>> renderType, NonNullConsumer<? super G> connectedTextures,
+							NonNullBiConsumer<DataGenContext<Block, G>, RegistrateBlockstateProvider> stateProvider) {
+								name += "_pane";
+								
+								return CreateDecoMod.REGISTRATE.block(name, factory)
+								.onRegister(connectedTextures)
+								.addLayer(renderType)
+								.initialProperties(() -> Blocks.GLASS_PANE)
+								.properties(p -> p.mapColor(parent.get()
+								.defaultMapColor()))
+								.blockstate(stateProvider)
+								.recipe((c, p) -> ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, c.get(), 16)
+								.pattern("###")
+								.pattern("###")
+								.define('#', parent.get())
+								.unlockedBy("has_ingredient", RegistrateRecipeProvider.has(parent.get()))
+								.save(p::accept))
+								.loot((t, g) -> t.dropWhenSilkTouch(g))
+								.item()
+								.model((c, p) -> p.withExistingParent(c.getName(), CreateDecoMod.id("item/pane"))
+								.texture("pane", sideTexture)
+								.texture("edge", topTexture))
+								.build()
+								.register();
+							}
+							
+							private static String palettesDir() {
+								return "block/palettes/windows/";
+							}
+						}
+						
